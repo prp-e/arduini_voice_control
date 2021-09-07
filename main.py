b39ef4cb338eb63fe10d5737f24985e75eb94698
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-09-07 11:33:35 +0430

    Voice commands set for another project.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import serial
 import speech_recognition as sr
 import time 
 
-target = serial.Serial('/dev/cu.usbmodem142101', 9600)
+target = serial.Serial('/dev/cu.usbmodem142201', 9600)
 
 def write_serial(input_text):
     target.write(input_text.encode())
@@ -13,7 +13,7 @@ recognizer = sr.Recognizer()
 
 def get_voice_command():
     with sr.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source)
+        recognizer.pause_threshold = 0.5
         audio = recognizer.listen(source)
         try:
             text = recognizer.recognize_google(audio, language='fa')
@@ -25,9 +25,9 @@ while True:
     print("Waiting for input")
     input_text = get_voice_command()
     print(input_text)
-    if input_text == 'روشن':
-        write_serial('on\n')
-    elif input_text == 'خاموش':
-        write_serial('off\n')
+    if input_text == 'قرمز':
+        write_serial('r')
+    elif input_text == 'زرد':
+        write_serial('y')
     elif input_text == 'خداحافظ':
         exit()
